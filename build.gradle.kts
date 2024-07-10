86cdfe19517d6c49602bbb378ea8c7a705580bff
liyuehui <lyh3874@qq.com>
2024-07-10 22:30:00 +0800

    删除无用代码

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,4 @@
-plugins {
-    kotlin("jvm") version "2.0.0"
-}
 
-group = "com.superman.poet"
-version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
 
-dependencies {
-    implementation(project(":poetlib"))
-}
 
-kotlin {
-    jvmToolchain(11)
-}
--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.superman.poet
-
-fun main() {
-    println("Hello World!")
-}
